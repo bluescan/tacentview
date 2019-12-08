03655bf221435a4960ff4ad7e0cd33c7600bf7f9
Tristan <bluescan@github.com>
2019-12-08 13:59:46 -0800

    Log window code cleanup. Made sure LTCG working in release config.

--- Src/TacitTexView.cpp
+++ Src/TacitTexView.cpp
@@ -34,100 +34,113 @@ namespace TacitTexView
 	int MajorVersion	= 0;
 	int MinorVersion	= 8;
 	int Revision		= 0;
+
+	// This class is a version of the one that ships with Dear ImGui.
+	struct ImGuiLog
+	{
+		ImGuiLog()																										: ScrollToBottom(true) { Clear(); }
+
+		void Clear();
+		void AddLog(const char* fmt, ...) IM_FMTARGS(2);
+		void Draw(const char* title, bool* popen = nullptr);
+
+		ImGuiTextBuffer Buf;
+		ImGuiTextFilter Filter;
+		ImVector<int> LineOffsets;			// Index to lines offset. We maintain this with AddLog() calls, allowing us to have a random access on lines
+		bool ScrollToBottom;
+	};
+
 };
 
-struct TextureViewerLog
+
+void TacitTexView::ImGuiLog::Clear()
 {
-	ImGuiTextBuffer Buf;
-	ImGuiTextFilter Filter;
-	ImVector<int> LineOffsets;			// Index to lines offset. We maintain this with AddLog() calls, allowing us to have a random access on lines
-	bool ScrollToBottom;
-	TextureViewerLog() : ScrollToBottom(true)
-	{
+	Buf.clear();
+	LineOffsets.clear();
+	LineOffsets.push_back(0);
+}
+
+
+void TacitTexView::ImGuiLog::AddLog(const char* fmt, ...)
+{
+	int oldSize = Buf.size();
+	va_list args;
+	va_start(args, fmt);
+	Buf.appendfv(fmt, args);
+	va_end(args);
+
+	for (int newSize = Buf.size(); oldSize < newSize; oldSize++)
+		if (Buf[oldSize] == '\n')
+			LineOffsets.push_back(oldSize + 1);
+
+	ScrollToBottom = true;
+}
+
+
+void TacitTexView::ImGuiLog::Draw(const char* title, bool* popen)
+{
+	if (ImGui::Button("Clear"))
 		Clear();
-	}
 
-	void Clear()
+	ImGui::SameLine();
+	bool copy = ImGui::Button("Copy");
+	ImGui::SameLine();
+	Filter.Draw("Filter", -100.0f);
+	ImGui::Separator();
+	ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
+	if (copy)
+		ImGui::LogToClipboard();
+
+	ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+	const char* buf = Buf.begin();
+	const char* bufEnd = Buf.end();
+	if (Filter.IsActive())
 	{
-		Buf.clear();
-		LineOffsets.clear();
-		LineOffsets.push_back(0);
-	}
-
-	void AddLog(const char* fmt, ...) IM_FMTARGS(2)
-	{
-		int old_size = Buf.size();
-		va_list args;
-		va_start(args, fmt);
-		Buf.appendfv(fmt, args);
-		va_end(args);
-		for (int new_size = Buf.size(); old_size < new_size; old_size++)
-			if (Buf[old_size] == '\n')
-				LineOffsets.push_back(old_size + 1);
-		ScrollToBottom = true;
-	}
-
-	void Draw(const char* title, bool* p_open = NULL)
-	{
-		if (ImGui::Button("Clear")) Clear();
-		ImGui::SameLine();
-		bool copy = ImGui::Button("Copy");
-		ImGui::SameLine();
-		Filter.Draw("Filter", -100.0f);
-		ImGui::Separator();
-		ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
-		if (copy)
-			ImGui::LogToClipboard();
-
-		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
-		const char* buf = Buf.begin();
-		const char* buf_end = Buf.end();
-		if (Filter.IsActive())
+		for (int lineNo = 0; lineNo < LineOffsets.Size; lineNo++)
 		{
-			for (int line_no = 0; line_no < LineOffsets.Size; line_no++)
+			const char* lineStart = buf + LineOffsets[lineNo];
+			const char* lineEnd = (lineNo + 1 < LineOffsets.Size) ? (buf + LineOffsets[lineNo + 1] - 1) : bufEnd;
+			if (Filter.PassFilter(lineStart, lineEnd))
+				ImGui::TextUnformatted(lineStart, lineEnd);
+		}
+	}
+	else
+	{
+		// The simplest way to display the entire buffer is with ImGui::TextUnformatted(buf, buf_end); TextUnformatted
+		// has specialization for large blobs of text and will fast-forward to skip non-visible lines. Here we instead
+		// demonstrate using the clipper to only process lines that are within the visible area. If you have tens of
+		// thousands of items and their processing cost is non-negligible, coarse clipping them on your side is
+		// recommended.
+		//
+		// ImGuiListClipper requires a) random access into your data, and b) items all being the same height, both of
+		// which we can handle since we an array pointing to the beginning of each line of text. When using the filter
+		// (in the block of code above) we don't have random access into the data to display anymore, which is why we
+		// don't use the clipper. Storing or skimming through the search result would make it possible and would be
+		// recommended if you want to search through tens of thousands of entries.
+		ImGuiListClipper clipper;
+		clipper.Begin(LineOffsets.Size);
+		while (clipper.Step())
+		{
+			for (int lineNo = clipper.DisplayStart; lineNo < clipper.DisplayEnd; lineNo++)
 			{
-				const char* line_start = buf + LineOffsets[line_no];
-				const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
-				if (Filter.PassFilter(line_start, line_end))
-					ImGui::TextUnformatted(line_start, line_end);
+				const char* lineStart = buf + LineOffsets[lineNo];
+				const char* lineEnd = (lineNo + 1 < LineOffsets.Size) ? (buf + LineOffsets[lineNo + 1] - 1) : bufEnd;
+				ImGui::TextUnformatted(lineStart, lineEnd);
 			}
 		}
-		else
-		{
-			// The simplest and easy way to display the entire buffer:
-			//ImGui::TextUnformatted(buf, buf_end); 
-			// And it'll just work. TextUnformatted() has specialization for large blob of text and will fast-forward to skip non-visible lines.
-			// Here we instead demonstrate using the clipper to only process lines that are within the visible area.
-			// If you have tens of thousands of items and their processing cost is non-negligible, coarse clipping them on your side is recommended.
-			// Using ImGuiListClipper requires A) random access into your data, and B) items all being the  same height, 
-			// both of which we can handle since we an array pointing to the beginning of each line of text.
-			// When using the filter (in the block of code above) we don't have random access into the data to display anymore, which is why we don't use the clipper.
-			// Storing or skimming through the search result would make it possible (and would be recommended if you want to search through tens of thousands of entries)
-			ImGuiListClipper clipper;
-			clipper.Begin(LineOffsets.Size);
-			while (clipper.Step())
-			{
-				for (int line_no = clipper.DisplayStart; line_no < clipper.DisplayEnd; line_no++)
-				{
-					const char* line_start = buf + LineOffsets[line_no];
-					const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
-					ImGui::TextUnformatted(line_start, line_end);
-				}
-			}
-			clipper.End();
-		}
-		ImGui::PopStyleVar();
-
-		if (ScrollToBottom)
-			ImGui::SetScrollHereY(1.0f);
-		ScrollToBottom = false;
-		ImGui::EndChild();
+		clipper.End();
 	}
-};
+	ImGui::PopStyleVar();
+
+	if (ScrollToBottom)
+		ImGui::SetScrollHereY(1.0f);
+	ScrollToBottom = false;
+	ImGui::EndChild();
+}
 
 
 static bool gLogOpen = true;
-static TextureViewerLog gLog;
+static TacitTexView::ImGuiLog gLog;
 
 
 void ShowTextureViewerLog(float x, float y, float w, float h)
