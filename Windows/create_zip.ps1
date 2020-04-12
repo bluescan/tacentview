0c9d8f99f7b0d48f7c23040a7a9dcb60ed3fffe1
Tristan <19311462+bluescan@users.noreply.github.com>
2020-04-11 18:57:38 -0700

    Powershell script to make windows zip file for releases. Just like the Ubuntu bersion, it parses the Version.h file.

---
+++ Windows/create_zip.ps1
@@ -0,0 +1,34 @@
+$file = "../Src/Version.h"
+
+foreach($line in (Get-Content $file))
+{
+	$splitLine = $line.Split("=")
+	if ($splitLine -and ($splitLine.Count -eq 2) )
+	{
+		$names = $splitLine.split(" `n`t",[System.StringSplitOptions]::RemoveEmptyEntries)
+		$name = $names[2]
+		$value = $splitLine[1].Trim(" ", "`n", "`t", ";")
+		New-Variable -Name $name -Value $value
+	}
+}
+
+$VersionString = $MajorVersion + "." + $MinorVersion + "." + $Revision
+Write-Output $VersionString
+
+$PackageName="tacittexview_${VersionString}"
+Write-Output $PackageName
+if (Test-Path $PackageName)
+{
+	Remove-Item $PackageName -Recurse
+}
+New-Item $PackageName -itemtype directory
+New-Item "$($PackageName)\\Data" -itemtype directory
+
+Copy-Item -Path "..\\TacitTexView.exe" -Destination "$($PackageName)\\"
+
+# png txt ttf 
+Copy-Item -Path "..\\Data\\*.png" -Destination "$($PackageName)\\Data"
+Copy-Item -Path "..\\Data\\*.txt" -Destination "$($PackageName)\\Data"
+Copy-Item -Path "..\\Data\\*.ttf" -Destination "$($PackageName)\\Data"
+
+Compress-Archive -Path $PackageName -DestinationPath "$($PackageName).zip"
