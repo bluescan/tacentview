b2949b026f4f894448adb4a7e42f46e0072765cc
Tristan <19311462+bluescan@users.noreply.github.com>
2020-11-08 03:27:40 -0800

    Display LibPNG version in about dialog.

--- Src/Dialogs.cpp
+++ Src/Dialogs.cpp
@@ -190,6 +190,7 @@ void Viewer::ShowAboutPopup(bool* popen)
 		ImGui::Text("ZLib V %s", tImage::Version_ZLIB);
 		ImGui::Text("Gif Load");
 		ImGui::Text("WebP Decoder V %d.%d", tImage::Version_WEBP_Major, tImage::Version_WEBP_Minor);
+		ImGui::Text("LibPNG V %s", tImage::Version_LibPNG);
 	}
 	ImGui::End();
 }
